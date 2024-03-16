15861ee1baaee0de002fabc99511cb8148dd259e
SureshNaidu8797 <141650899+SureshNaidu8797@users.noreply.github.com>
2024-03-17 02:28:33 +0530

    Time: 1 ms (98.65%) | Memory: 41.7 MB (19.84%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,20 +1,33 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
-            st.push(s.charAt(i));   
-            continue;  
-            }
-            if(st.size()==0)
-            return false;
-            else if( (s.charAt(i)==')' && st.pop()=='(') || (s.charAt(i)==']' &&st.pop()=='[') || (s.charAt(i)=='}' &&st.pop()=='{') )
-            continue;
-            else
-            return false;
-        }
-        if(st.size()==0)
-        return true;
+        // Stack<Character> st=new Stack<>();
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+        //     st.push(s.charAt(i));   
+        //     continue;  
+        //     }
+        //     if(st.size()==0)
+        //     return false;
+        //     else if( (s.charAt(i)==')' && st.pop()=='(') || (s.charAt(i)==']' &&st.pop()=='[') || (s.charAt(i)=='}' &&st.pop()=='{') )
+        //     continue;
+        //     else
+        //     return false;
+        // }
+        // if(st.size()==0)
+        // return true;
+        // return false;
+
+    Stack<Character> stack = new Stack<>();
+    for (final char c : s.toCharArray())
+      if (c == '(')
+        stack.push(')');
+      else if (c == '{')
+        stack.push('}');
+      else if (c == '[')
+        stack.push(']');
+      else if (stack.isEmpty() || stack.pop() != c)
         return false;
+
+    return stack.isEmpty();
     }
 }
