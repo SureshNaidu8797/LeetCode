a0e70032865f1bb7dfa635484c9bd5ff79e05285
SureshNaidu8797 <141650899+SureshNaidu8797@users.noreply.github.com>
2024-03-17 01:40:20 +0530

    Time: 23 ms (79.94%) | Memory: 55.1 MB (54.73%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -4,10 +4,7 @@ class Solution {
         HashMap<Integer,Integer> freq=new HashMap<>();
         freq.put(0,-1);
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0)
-                s-=1;
-            else
-                s+=1;
+            s+=nums[i]==1 ? 1:-1;
             if(freq.containsKey(s))
                 maxCount=Math.max(maxCount,i-freq.get(s));
             else
