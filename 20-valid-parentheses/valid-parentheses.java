2b3899c0bd87bcefee6b4a857a3d2817cadacdf4
SureshNaidu8797 <141650899+SureshNaidu8797@users.noreply.github.com>
2024-03-17 02:21:22 +0530

    Time: 2 ms (54.10%) | Memory: 41.4 MB (64.23%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+            st.push(s.charAt(i));   
+            continue;  
+            }
+            if(st.size()==0)
+            return false;
+            if(s.charAt(i)==')' && st.pop()=='(')
+            continue;
+            else if(s.charAt(i)=='}' &&st.pop()=='{')
+            continue;
+            else if(s.charAt(i)==']' &&st.pop()=='[')
+            continue;
+            else
+            return false;
+        }
+        if(st.size()==0)
+        return true;
+        return false;
+    }
+}
