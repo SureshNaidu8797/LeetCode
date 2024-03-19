7fa0829a038a7933a0d677c2b054a13d1aa584dc
SureshNaidu8797 <141650899+SureshNaidu8797@users.noreply.github.com>
2024-03-19 22:32:06 +0530

    Time: 2 ms (99.12%) | Memory: 45.4 MB (83.40%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -6,13 +6,16 @@ class Solution {
         int max = 0;
         for (char task : tasks) {
             freq[task - 'A']++;
-            max = Math.max(max, freq[task - 'A']);
+            // max = Math.max(max, freq[task - 'A']);
         }
         int numOfElementsWithMaxFreq = 0;
         for (int frequency : freq) {
-            if (frequency == max) {
-                numOfElementsWithMaxFreq++;
+            if (frequency > max) {
+                max=frequency;
+                numOfElementsWithMaxFreq=1;
             }
+            else if(frequency==max)
+            numOfElementsWithMaxFreq++;
         }
         return Math.max((n + 1) * (max - 1) + numOfElementsWithMaxFreq, tasks.length);
     }
