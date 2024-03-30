489e01b582864a898d74de627e80cfe7ff8e97c6
SureshNaidu8797 <141650899+SureshNaidu8797@users.noreply.github.com>
2024-03-30 20:16:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (83.09%) - LeetSync

--- 1310-watering-plants/watering-plants.java
+++ 1310-watering-plants/watering-plants.java
@@ -1,10 +1,17 @@
 class Solution {
     public int wateringPlants(int[] plants, int capacity) {
-        int steps=0, n=plants.length ,cap=capacity;
-        for(int i=0;i<n;i++){
-            steps+=(plants[i]<=cap ? 0 : 2) *i+1;
-            cap=(plants[i]<=cap ? cap : capacity) -plants[i];
+        int count=0;
+        int c=capacity;
+        for(int i=0;i<plants.length;i++){
+            if(c>=plants[i]){
+                c=c-plants[i];
+                count++;
+            }
+            else{
+                count=count+i+i+1;
+                c=capacity-plants[i];
+            }
         }
-        return steps;
+        return count;
     }
 }
