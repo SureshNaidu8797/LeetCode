00ad99ed849453f26ed7a08cf5dd2fc7887a70de
SureshNaidu8797 <141650899+SureshNaidu8797@users.noreply.github.com>
2024-04-03 15:51:32 +0530

    Time: 146 ms (57.37%) | Memory: 41.3 MB (74.16%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,36 +1,39 @@
-class Solution {
+public class Solution {
     public boolean exist(char[][] board, String word) {
-        int rows = board.length;
-        int cols = board[0].length;
+        int m = board.length;
+        int n = board[0].length;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (solve(board, rows, cols, i, j, word, 0))
+        boolean[][] visited = new boolean[m][n];
+        boolean result = false;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0) && backtrack(board, word, visited, i, j, 0))
                     return true;
             }
         }
         return false;
     }
-
-    int[] rowDirections = { 1, -1, 0, 0 };
-    int[] colDirections = { 0, 0, 1, -1 };
-
-    public boolean solve(char[][] board, int rows, int cols, int row, int col, String word, int index) {
-        if (index == word.length())
+    
+    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+        if (index == word.length()) {
             return true;
-        if (row < 0 || row >= rows || col < 0 || col >= cols || board[row][col] != word.charAt(index))
-            return false;
-        char temp = board[row][col];
-        board[row][col] = '.';
-
-        for (int d = 0; d < 4; d++) {
-            int newRow = row + rowDirections[d];
-            int newCol = col + colDirections[d];
-            if (solve(board, rows, cols, newRow, newCol, word, index + 1))
-                return true;
         }
-
-        board[row][col] = temp;
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+        
+        visited[i][j] = true;
+        
+        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
+            backtrack(board, word, visited, i - 1, j, index + 1) ||
+            backtrack(board, word, visited, i, j + 1, index + 1) ||
+            backtrack(board, word, visited, i, j - 1, index + 1)) {
+            return true;
+        }
+        
+        visited[i][j] = false;
         return false;
     }
 }
