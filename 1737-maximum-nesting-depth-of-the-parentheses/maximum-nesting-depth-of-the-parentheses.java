0006a1e6291e048312fa61951726819a0c95ac7c
SureshNaidu8797 <141650899+SureshNaidu8797@users.noreply.github.com>
2024-04-04 15:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (40.14%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxDepth(String s) {
+        int count=0,maxCount=0;
+        for(char c:s.toCharArray()){
+            if(c=='('){
+                count++;
+                if(maxCount<count)
+                    maxCount=count;
+            }
+            else if(c==')')
+            count--;
+        }
+        return maxCount;
+    }
+}
