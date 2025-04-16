43a31fd8a8a880f1223eb99c0238f93d2488bec2
SureshNaidu8797 <141650899+SureshNaidu8797@users.noreply.github.com>
2025-04-16 22:40:17 +0530

    Time: 6 ms (97.57%), Space: 48 MB (80.46%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -1,31 +1,33 @@
 class Solution {
-    public int findSum(int[] nums, int div){
-        int sum =0;
-        for(int n: nums){
-            sum+= Math.ceil((double)n/div);
+    static int validSome(int num, int nums[])
+    {
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum+=(nums[i]+num-1)/num;
         }
         return sum;
     }
-    public int binarySearch(int[]nums, int threshold, int low, int high){
-
-        while(low<= high){
-            int mid  = (low+high)/2;
-            if(findSum(nums, mid)>threshold){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-        return low;
-    }
     public int smallestDivisor(int[] nums, int threshold) {
-        int max = Integer.MIN_VALUE;
-        for(int n: nums){            
-            if(n>max){
-                max=n;
-            }
+        
+        int high=0;
+        int low=1;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>high) high=nums[i];
         }
-        return binarySearch(nums, threshold, 1, max);
+        int ans=0;
+        while(low<=high)
+        {
+            int mid=(2*low+high-low)/2;
+            int resultSum=validSome(mid, nums);
+            if(resultSum<=threshold)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
     }
 }
